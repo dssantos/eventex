abef719a1cce2b036a949d40ca12685b8ca6db79
Danilo Santos <danilosantos1986@gmail.com>
2019-12-28 11:40:43 -0300

    Thank you page

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -47,8 +47,8 @@ class SubscribePostValid(TestCase):
         self.resp = self.client.post('/inscricao/', data)
 
     def test_post(self):
-        """Valid post should redirect to /inscricao/"""
-        self.assertEqual(302, self.resp.status_code)  # 302 é o status code de redirect
+        """Valid post should redirect to /inscricao/1/"""
+        self.assertRedirects(self.resp, '/inscricao/1/')
 
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
@@ -78,13 +78,3 @@ class SubscribePostInvalid(TestCase):
 
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
-
-
-class SubscribeSuccessMessage(TestCase):
-    def test_message(self):
-        data = dict(name='Danilo Santos',
-                    cpf='12345678901',
-                    email='danilo@oi.com',
-                    phone='71-99999-9999')
-        response = self.client.post('/inscricao/', data, follow=True)
-        self.assertContains(response, 'Inscrição realizada com sucesso!')
